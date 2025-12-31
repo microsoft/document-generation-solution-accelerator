524c3c356c9052e3791461e79f10d4ab5a7fc740
Ragini-Microsoft <v-raginich@microsoft.com>
2025-12-31 13:46:56 +0530

    remove execute permission commands from setup_env.sh

--- .devcontainer/setup_env.sh
+++ .devcontainer/setup_env.sh
@@ -2,9 +2,3 @@
 
 git fetch
 git pull
-
-# provide execute permission to deploy scripts
-sudo chmod +x ./content-gen/scripts/deploy.sh
-sudo chmod +x ./content-gen/scripts/local_dev.sh
-sudo chmod +x ./content-gen/scripts/deploy.ps1
-sudo chmod +x ./content-gen/scripts/local_dev.ps1
